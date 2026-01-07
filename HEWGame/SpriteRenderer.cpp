9cbace2d1a26df2d3f757f6a26861705f5127f2e
yuuki <yu04ki23123@gmail.com>
2026-01-07 14:11:40 +0900

    Revert "テスト"
    This reverts commit 679d7c0f8811439853dad3ed4cb3a1efed578e48.

--- HEWGame/SpriteRenderer.cpp
+++ HEWGame/SpriteRenderer.cpp
@@ -47,7 +47,7 @@ HRESULT SpriteRenderer::Init(ID3D11Device* pDevice)
     D3D11_INPUT_ELEMENT_DESC layout[] = 
     {
         { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-        { "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 8, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+        { "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
     };
     pDevice->CreateInputLayout(layout, ARRAYSIZE(layout), pVSBlob->GetBufferPointer(), pVSBlob->GetBufferSize(), &m_pInputLayout);
     pVSBlob->Release();
