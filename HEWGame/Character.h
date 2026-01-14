1079d1d19a3ccdbdf8647adc9168873880534735
Sakagami-Minson <minson.yuaks23@gmail.com>
2026-01-14 11:48:47 +0900

    PlayerとCharacter変えた

--- HEWGame/Character.h
+++ HEWGame/Character.h
@@ -6,11 +6,21 @@ class Character : public GameObject
 protected:
     float m_Speed;      // 移動速度
     int   m_HP;         // 体力
+
+    const float gravity;
+
+    bool isGround = false;
     // その他、ジャンプ力や向きなどキャラクター共通の変数をここに追加
 
 public:
     Character();
     virtual ~Character() {}
+
+    virtual void UnInit() = 0;
+
+    virtual void Move() = 0;
+    virtual void Attack() = 0;
+    virtual void Jump() = 0;
 
     // キャラクター共通の初期化（必要であれば）
     virtual void Init(ID3D11ShaderResourceView* pTexture) override 
--- HEWGame/Player.h
+++ HEWGame/Player.h
@@ -1,7 +1,51 @@
 #pragma once
 #include "Character.h"
+#include"Input.h"
+
+enum class PState
+{
+	STAY,				//止まっている
+	MOVE,				//移動
+	JUMP,				//ジャンプ
+	ATTACK,				//攻撃
+	STAN,				//気絶
+
+	//NODAMAGE,			//無敵
+	//WALL_GRAB,			//壁に
+	//BRINK,				//ブリンク
+
+};
+enum PDirection
+{
+	NONE,				//動きなし
+	RIGHT,				//右
+	LEFT,				//左
+};
+
 class Player :public Character
 {
+private:
+
+	//プレイヤーの状態
+	PState state = PState::STAY;
+	PDirection dir = NONE;
+
+	//ジャンプ関連
+	bool isJump = false;  //ジャンプしているかどうか
+	float jumpPower = 0.0f; //ジャンプ初速
+	float velocityY = 0.0f;  //ジャンプ速度
+
+	//入力
+	Input input;
+	//GameObject m_player;
+
+
+	bool isBlink = false;
+	bool isHit = false;
+	bool isWall = false;
+	bool isWallJump = false;
+	bool isDodge = false;
+
 
 public:
     // コンストラクタ・デストラクタ
@@ -13,4 +57,16 @@ public:
 
     // 毎フレームの更新処理（入力による移動など）
     void Update() override;
+
+
+	void Draw(ID3D11DeviceContext* pContext, SpriteRenderer* pSR, DirectX::XMMATRIX viewProj)override;
+	void UnInit()override;
+
+	void Move()override;
+	void Jump()override;
+	void Attack()override;
+
+	void WallJump();
+	void Blink();
+	void GetBlink();
 };
