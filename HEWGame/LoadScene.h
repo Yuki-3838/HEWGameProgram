e858a41b4fe7d7fdafe2975c23889ab1e2756d02
yuuki <yu04ki23123@gmail.com>
2026-01-24 00:44:27 +0900

    いったんコミット

--- HEWGame/LoadScene.h
+++ HEWGame/LoadScene.h
@@ -1,1 +1,35 @@
 #pragma once
+#include "Scene.h"
+#include <thread>
+#include <atomic> 
+
+enum class NextSceneType
+{
+    Stage1,
+    Title,
+};
+
+class LoadScene : public Scene
+{
+private:
+    ID3D11ShaderResourceView* m_pLoadTex = nullptr; // ローディング画像
+    NextSceneType m_NextScene;
+
+    //マルチスレッド用
+    std::thread* m_pThread = nullptr;      // 裏で作業するスレッド
+    std::atomic<bool> m_IsFinished;        // 「終わった？」を管理するフラグ（スレッドセーフ）
+
+    // 裏で実行する関数
+    void BackgroundLoad();
+
+public:
+    LoadScene(Renderer* r, ResourceManager* res, SpriteRenderer* spr, Input* input, NextSceneType next);
+    ~LoadScene() override {}
+
+    void Init() override;
+    void Update() override;
+    void Draw() override;
+    void Uninit() override;
+    bool ShouldChangeScene() const override { return m_IsFinished; }
+    NextSceneType GetNextSceneType() const { return m_NextScene; }
+};
